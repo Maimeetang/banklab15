b25066adb206a2ea6e5e40fe006dcb6da3a1e76f
Bank <sirawit.36298@gmail.com>
2023-01-17 17:09:53 +0700

    lab15_2 completed

--- lab15_2.cpp
+++ lab15_2.cpp
@@ -19,3 +19,41 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+
+void inputMatrix(double A[][N]){
+	int Row = 1;
+	for(int i = 0; i<N;i++){
+		cout << "Row " << Row << ": ";
+		for(int j=0;j<N;j++){
+			double x;
+			cin >> x;
+			A[i][j] = x;
+		}
+		Row++;
+	}
+}
+
+void findLocalMax(const double A[][N], bool B[][N]){
+	for(int i = 0; i<N;i++){
+		for(int j=0;j<N;j++){
+			if(i>0 and j>0 and i<N-1 and j<N-1){
+				if(A[i][j] < A[i-1][j] or A[i][j] < A[i+1][j] or A[i][j] < A[i][j-1] or A[i][j] < A[i][j+1]){
+					B[i][j] = false;
+				}else{
+					B[i][j] = true;
+				}
+			}else{
+				B[i][j] = false;
+			}
+		}
+	}
+}
+
+void showMatrix(const bool A[][N]){
+	for(int i = 0; i<N;i++){
+		for(int j=0;j<N;j++){
+			cout << A[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
